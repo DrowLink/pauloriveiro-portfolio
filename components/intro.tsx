b62ef507aefde8b014839428e72c35033d80db87
drowlink <pauloriveiro01@gmail.com>
2023-08-14 16:17:24 -0400

    new sections added

--- components/intro.tsx
+++ components/intro.tsx
@@ -21,13 +21,13 @@ export default function Intro() {
             </div>
         </div>
 
-        <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
+        <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
             <span className="font-bold">Hello, I'm Paulo.</span> I'm a{" "}
             <span className="font-bold">full-stack developer</span> with{" "}
             <span className="font-bold">X years</span> of experience. I enjoy
             building <span className="italic">sites & apps</span>. My focus is{" "}
             <span className="underline">React (Next.js)</span>.
-        </motion.p>
+        </motion.h1>
 
         <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium' initial={{ opacity:0, y:100 }} animate={{ opacity: 1, y: 0}} transition={{ delay: 0.1, }}>
             <Link className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition" href="#contact">Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/> </Link>
