4e3c1c785cf788c7e91d7e33aaaa9b3490f3acad
drowlink <pauloriveiro01@gmail.com>
2023-08-14 21:10:05 -0400

    viewport navs

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,15 +1,30 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { motion } from "framer-motion"
 import Link from 'next/link'
 import { BsArrowRight, BsLinkedin } from "react-icons/bs"
 import { HiDownload } from "react-icons/hi"
 import { FaGithubSquare } from 'react-icons/fa'
+import { useActiveSectionContext } from '@/context/active-section-context'
+import { useInView } from 'react-intersection-observer'
+
+
 export default function Intro() {
+    const { ref, inView } = useInView({
+        threshold: 0.5,
+    })
+    const { setActiveSection } = useActiveSectionContext()
+  
+    useEffect(() => {
+      if (inView) {
+        setActiveSection("Home")
+      }
+    }, [inView, setActiveSection])
+
   return (
-    <section  id="home" className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
+    <section  ref={ref} id="home" className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
         <div className="flex items-center justify-center">
             <div className="relative">
                 <motion.div initial={{ opacity: 0, scale: 0}} animate={{ opacity: 1, scale: 1}} transition={{ type: "tween", duration: 0.2 }} className="">
--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -19,7 +19,7 @@ export const ActiveSectionContext = createContext<ActiveSectionContextType | nul
 export default function ActiveSectionContextProvider({ children }: 
     ActiveSectionContextProviderProps) {
     const [activeSection, setActiveSection] = useState<SectionName>("Home")
-
+    const [timeOfLastClick, setTimeOfLastClick] = useState(0);
 
   return (
   <ActiveSectionContext.Provider value={{ activeSection: activeSection, setActiveSection}}>
