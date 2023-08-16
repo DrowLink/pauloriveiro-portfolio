ed2edaec251c4084d687743586d1c4199ef0ed47
drowlink <pauloriveiro01@gmail.com>
2023-08-16 17:31:47 -0400

    first version

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -15,16 +15,16 @@ export default function Contact() {
   return (
     <motion.section ref={ref} id='contact' className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center' initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once: true}}>
         <SectionHeading>Contact me</SectionHeading>
-        <p className='text-gray-700 -mt-6'>Please contact me directly at <a className='underline' href='mailto:pauloriveiro01@gmail.com'>pauloriveiro01@gmail.com</a> or through this form.</p>
-        <form className='mt-10 flex flex-col' action={async (formData) => { const { data, error } = await sendEmail(formData) 
+        <p className='text-gray-700 -mt-6 dark:text-white/80'>Please contact me directly at <a className='underline' href='mailto:pauloriveiro01@gmail.com'>pauloriveiro01@gmail.com</a> or through this form.</p>
+        <form className='mt-10 flex flex-col dark:text-black' action={async (formData) => { const { data, error } = await sendEmail(formData) 
         if (error) {
             toast.error(error)
             return;
         }
         toast.success("Email sent succesfully! 😎")
         }}>
-            <input className="h-14 px-4 rounded-lg border borderBlack" name="senderEmail" placeholder="Your email" required maxLength={500} type="email" />
-            <textarea className='h-52 my-3 rounded-lg borderBlack p-4' maxLength={5000} name="message" placeholder='Your message'/>
+            <input className="h-14 px-4 rounded-lg border borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" name="senderEmail" placeholder="Your email" required maxLength={500} type="email" />
+            <textarea className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' maxLength={5000} name="message" placeholder='Your message'/>
             <SubmitBtn />
         </form>
     </motion.section>)
