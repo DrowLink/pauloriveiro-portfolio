12d46623e8e41db8cca2aa3d8342d2bc48e77fd0
drowlink <pauloriveiro01@gmail.com>
2023-08-25 17:52:17 -0400

    changing file

--- components/intro.tsx
+++ components/intro.tsx
@@ -39,7 +39,7 @@ export default function Intro() {
 
         <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium' initial={{ opacity:0, y:100 }} animate={{ opacity: 1, y: 0}} transition={{ delay: 0.1, }}>
             <Link className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition" onClick={() => { setActiveSection("Contact"); setTimeOfLastClick(Date.now()) }} href="#contact">Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/> </Link>
-            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" href="/CV.pdf" download>Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/> </a>
+            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" href="/CV-PauloR.pdf" download>Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/> </a>
             <a className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60" href="https://www.linkedin.com/in/paulo-riveiro-74a5a218b/" target="_blank">
                 <BsLinkedin />
             </a>
