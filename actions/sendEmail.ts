c3d981043d928ca5e2d068ea1982fecc5bc4d7c0
drowlink <pauloriveiro01@gmail.com>
2023-11-18 15:47:29 -0400

    errores de upgrading

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,54 +1,54 @@
-"use server";
+// "use server";
 
-import React from 'react';
-import { Resend } from "resend";
-import { validateString, getErrorMessage } from "@/lib/utils";
-import ContactFormEmail from "@/email/contact-form-email";
+// import React from 'react';
+// import { Resend } from "resend";
+// import { validateString, getErrorMessage } from "@/lib/utils";
+// import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 
-export const sendEmail = async (formData: FormData) => {
-    const senderEmail = formData.get('senderEmail');
-    const message = formData.get('message');
+// export const sendEmail = async (formData: FormData) => {
+//     const senderEmail = formData.get('senderEmail');
+//     const message = formData.get('message');
 
-    console.log(senderEmail);
+//     console.log(senderEmail);
 
-    // simple server-side validation
-   if (!validateString(senderEmail, 500)) {
-    return {
-        error: "Invalid sender email"
-    }
-   }
-   if (!validateString(message, 5000)) {
-    return {
-        error: "invalid message"
-    }
-   }
+//     // simple server-side validation
+//    if (!validateString(senderEmail, 500)) {
+//     return {
+//         error: "Invalid sender email"
+//     }
+//    }
+//    if (!validateString(message, 5000)) {
+//     return {
+//         error: "invalid message"
+//     }
+//    }
 
-   new Error('message')
+//    new Error('message')
 
-   let data;
-   try {
-    data = await resend.emails.send({
-        from: 'Contact Form <onboarding@resend.dev>',
-        to: 'pauloriveiro01@gmail.com',
-        subject: 'Message from contact form',
-        reply_to: senderEmail as string,
-        react: React.createElement(ContactFormEmail, {
-            message: message as string,
-            senderEmail: senderEmail as string,
-        }) //same as below
-        // react: <ContactFormEmail message={message} senderEmail={senderEmail} />,
-    });
-   } catch (error: unknown) {
-        return {
-            error: getErrorMessage(error)
-        }
-    }
+//    let data;
+//    try {
+//     data = await resend.emails.send({
+//         from: 'Contact Form <onboarding@resend.dev>',
+//         to: 'pauloriveiro01@gmail.com',
+//         subject: 'Message from contact form',
+//         reply_to: senderEmail as string,
+//         react: React.createElement(ContactFormEmail, {
+//             message: message as string,
+//             senderEmail: senderEmail as string,
+//         }) //same as below
+//         // react: <ContactFormEmail message={message} senderEmail={senderEmail} />,
+//     });
+//    } catch (error: unknown) {
+//         return {
+//             error: getErrorMessage(error)
+//         }
+//     }
 
-    return {
-        data,
-    }
-}
+//     return {
+//         data,
+//     }
+// }
--- components/contacts.tsx
+++ components/contacts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { sendEmail } from '@/actions/sendEmail';
+// import { sendEmail } from '@/actions/sendEmail';
 import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion';
 import React from 'react';
@@ -16,14 +16,14 @@ export default function Contact() {
     <motion.section ref={ref} id='contact' className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center' initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 5}} viewport={{once: true}}>
         <SectionHeading>¡Contactáme! 📨</SectionHeading>
         <p className='text-gray-700 -mt-6 dark:text-white/80'>Póngase en contacto conmigo al <a className='underline' href='mailto:pauloriveiro01@gmail.com'>pauloriveiro01@gmail.com</a> o a través de este formulario.</p>
-        <form className='mt-10 flex flex-col dark:text-black' action={async (formData) => { const { data, error } = await sendEmail(formData) 
-        if (error) {
-            toast.error(error)
-            return;
-        }
+        <form className='mt-10 flex flex-col dark:text-black'
+        // if (error) {
+        //     toast.error(error)
+        //     return;
+        // }
         // toast.success("Email sent succesfully! 😎✅")
-        toast.success("Correo enviado exitosamente! 😎✅")
-        }}>
+        // toast.success("Correo enviado exitosamente! 😎✅")
+        >
             <input className="h-14 px-4 rounded-lg border borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" name="senderEmail" placeholder="Your email" required maxLength={500} type="email" />
             <textarea className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' maxLength={5000} name="message" placeholder='Your message'/>
             <SubmitBtn />
