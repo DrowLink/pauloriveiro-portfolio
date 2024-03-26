945a81aee4dab67c8368026230b187c5d517b2c7
Paulo Riveiro <pauloriveiro01@gmail.com>
2024-03-26 02:23:21 +0000

    copyright changed

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className='mb-10 px-4 text-center text-gray-500'>
         <small className='mb-2 block text-xs'>
             {/* &copy; 2023 Paulo Riveiro. All rights reserved. */}
-            &copy; 2023 Paulo Riveiro. Todos los derechos reservados.
+            &copy; 2024 Paulo Riveiro. Todos los derechos reservados.
         </small>
         <p className='text-xs'>
             <span className='font-semibold'>Sobre este sitio web: </span> Construido con React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
